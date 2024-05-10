f07b0bb0251a53cb68aafac50518944c2704192f
JandersonMota <jandersonyo@gmail.com>
2024-05-10 16:32:33 -0300

    Assunto sobre Operador Ternario.

---
+++ src/main/java/edu/janderson/tema/ternario/OperadorTernario.java
@@ -0,0 +1,11 @@
+package main.java.edu.janderson.tema.ternario;
+
+public class OperadorTernario {
+  public static void main(String[] args) {
+    int nota = 5;
+
+    String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+    System.out.println(resultado);
+  }
+}
---
+++ src/main/java/edu/janderson/tema/ternario/OperadorTernarioComControleDeFluxoEncadeada.java
@@ -0,0 +1,11 @@
+package main.java.edu.janderson.tema.ternario;
+
+public class OperadorTernarioComControleDeFluxoEncadeada {
+  public static void main(String[] args) {
+    int nota = 5;
+
+    String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+    System.out.println(resultado);
+  }
+}
