00050b027adb9d2312b82914600d72d9b827a51b
JandersonMota <jandersonyo@gmail.com>
2024-05-15 00:22:13 -0300

    Restaurando pacote

---
+++ src/main/java/edu/janderson/tema/estr_condicional/ternario/OperadorTernario.java
@@ -0,0 +1,11 @@
+package main.java.edu.janderson.tema.estr_condicional.ternario;
+
+public class OperadorTernario {
+  public static void main(String[] args) {
+    int nota = 5;
+
+    String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+    System.out.println(resultado);
+  }
+}
