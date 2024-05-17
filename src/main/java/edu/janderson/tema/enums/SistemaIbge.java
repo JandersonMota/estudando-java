649a0cc67f0c3484759c940cbb9d2c5198220eeb
JandersonMota <jandersonyo@gmail.com>
2024-05-17 19:41:15 -0300

    -

--- src/main/java/edu/janderson/tema/enums/SistemaIbge.java
+++ src/main/java/edu/janderson/tema/enums/SistemaIbge.java
@@ -5,5 +5,11 @@ public class SistemaIbge {
     for(EstadoBrasileiro e: EstadoBrasileiro.values()) {
       System.out.println(e.getSigla() + " - " + e.getNome());
     }
+
+    EstadoBrasileiro estado = EstadoBrasileiro.PIAUI;
+
+    System.out.println("\nNome = " + estado.getNome());
+    System.out.println("Sigla = " + estado.getSigla());
+    System.out.println("Nome Maiusculo = " + estado.getNomeMaiusculo());
   }
 }
